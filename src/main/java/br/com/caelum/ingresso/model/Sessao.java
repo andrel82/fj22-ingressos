541265f501e446fa1f6f5afe39460d48f350c4ae
andrel82 <andrelsds@yahoo.com.br>
2018-03-24 16:31:29 -0300

    Implementando sessao para salvar no banco

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,35 @@
 package br.com.caelum.ingresso.model;
 
-public class Sessao {
+import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Sessao {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private LocalTime horario;
+	
+	@ManyToOne
+	private Sala sala;
+	
+	@ManyToOne
+	private Filme filme;
+
+//Construtor Default para iniciar Hibernate
+public Sessao(){}
+	
+public Sessao(LocalTime horario, Filme filme, Sala sala){
+	this.horario = horario;
+	this.filme = filme;
+	this.sala = sala;
+}
+
+public LocalTime getHorarioTermino(){
+	return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+}
 }
